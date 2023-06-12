0fa3623bb10d6b2e5b16c42b38c76a25e8735f2b
bmuraya <babumuraya@gmail.com>
2023-06-12 21:52:25 +0300

    test add doc

---
+++ ex11.py
@@ -0,0 +1,20 @@
+print ("How old are you?", end='')
+age= input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+#sample 
+
+name = input("Please enter your name: ")
+age = int(input("Please enter your age: "))
+email = input("Please enter your email address: ")
+
+print("Form submitted:")
+print("_________________________")
+print("Name:", name)
+print("Age:", age)
+print("Email:", email)

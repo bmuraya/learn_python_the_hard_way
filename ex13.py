6231d278b89d83eb4e37d7e79058356d5e581d50
bmuraya <babumuraya@gmail.com>
2023-06-13 21:35:16 +0300

    input prompt

---
+++ ex13.py
@@ -0,0 +1,12 @@
+from sys import argv 
+#read r=the WYSS section for now to run this 
+script, first, second, third, fourth = argv
+
+print ("The script is called:", script)
+print ("The first is called:", first)
+print ("The second is called:", second)
+print ("The third is called:", third)
+print ("The fourth is called:", fourth)
+
+user_input = input('add somr text here:')
+print(f"your input was: {user_input}.")
